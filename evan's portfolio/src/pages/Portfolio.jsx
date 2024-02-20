68396a867ca77ff14101f6b75a3c51452bf7f63b
evan-hatley <143963975+evan-hatley@users.noreply.github.com>
2024-02-19 22:06:41 -0600

    Portfolio
    Added 2 deployed projects to my Portfolio section. Still need to add styling to everything but now all of the file pathways work!

--- evan's portfolio/src/pages/Portfolio.jsx
+++ evan's portfolio/src/pages/Portfolio.jsx
@@ -1,5 +1,22 @@
 function Portfolio() {
     
+    return (
+        <div>
+          <div>
+            <h2>Career Canvas</h2>
+            <img src="/career-canvas.jpg" alt="Career Canvas" />
+            <p><a href="https://fierce-depths-59937-afaae183c587.herokuapp.com/login" target="_blank" rel="noopener noreferrer">Career Canvas</a></p>
+            <p><a href="https://github.com/evan-hatley/Career-Canvas" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+          </div>
+          <div>
+            <h2>JATE Text Editor</h2>
+            <img src="/JATE.jpg" alt="JATE" />
+            <p><a href="https://thawing-plains-70108-316d8b1956e0.herokuapp.com/" target="_blank" rel="noopener noreferrer">JATE Text Editor</a></p>
+            <p><a href="https://github.com/evan-hatley/PWA-Text-Editor" target="_blank" rel="noopener noreferrer">GitHub</a></p>
+          </div>
+        </div>
+      );
+
 }
 
 export default Portfolio;
